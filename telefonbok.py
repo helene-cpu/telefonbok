08cfc86aeb98796733828084da228eb32b955d9c
helene-cpu <66snahel@stud.akademiet.no>
2025-09-12 14:49:56 +0200

    Lage en funksjon som viser navn og nummer. Og endre fra alder til nummer.

--- telefonbok.py
+++ telefonbok.py
@@ -1,7 +1,14 @@
+#OPPGAVE 1
 telefonbok= []
 
-person1= {"navn": "Kari", "alder": 71}
-person2= {"navn": "Ola", "alder": 34}
+person1= {"navn": "Kari", "nummer": 71326643}
+person2= {"navn": "Ola", "nummer": 34894523}
 
 telefonbok.append(person1)
 telefonbok.append(person2)
+
+def vis_alle(person):
+    print(f"Navn: {person['navn']} : {person['nummer']}")
+
+for t in telefonbok:
+    vis_alle(t)
