68403ba23594a8967213ccde3c4b78c08a99d13e
helene-cpu <66snahel@stud.akademiet.no>
2025-09-15 12:41:52 +0200

    La til en funksjon for å legge til en ny person

--- telefonbok.py
+++ telefonbok.py
@@ -1,4 +1,4 @@
-#OPPGAVE 1
+#OPPGAVE 1, 2, 3, 4, 5 og 6
 telefonbok= []
 
 person1= {"navn": "Kari", "nummer": 71326643}
@@ -12,3 +12,15 @@ def vis_alle(person):
 
 for t in telefonbok:
     vis_alle(t)
+
+
+def legg_til():
+    navn= input(f"Skriv navn på personen du vil legge til: ")
+    nummer= input(f"Skriv nummeret til personen du akkurat skrev navnet på: ")
+
+    person3= {"navn": navn, "nummer": nummer}
+    telefonbok.append(person3)
+    print(f"{navn} ble lagt til i telefonboka")
+
+legg_til()
+
